617f2a634a0930c9b60d107c9a489f538c450650
Shas-gh <shaunchiangkw@gmail.com>
2026-07-26 23:30:54 +0800

    UI elements
    partially added ui elements to the battle screen, still requires lives hookup to hearts and the actual ui.

--- assets/scripts/transition.gd
+++ assets/scripts/transition.gd
@@ -1,9 +1,19 @@
 extends CanvasLayer
 
 @onready var transitionBlock: Control = $Transition
+@onready var clock = $Panel/Clock
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	transitionBlock.show()
 	var tween = create_tween()
 	tween.tween_property(transitionBlock, "position:x", -transitionBlock.size.x, 0.25)
+	
+	var round_manager = get_tree().get_first_node_in_group("round_manager");
+	if round_manager:
+		round_manager.time_updated.connect(update_timer);
+		
+func update_timer(seconds_left: int):
+	clock.text = str(seconds_left)
+
+	
