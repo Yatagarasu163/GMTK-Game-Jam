b8fab899608f1bf6742f2a1ebc810fd126324213
Kay Faraday <yatagarasu163@gmail.com>
2026-07-26 20:39:06 +0800

    yes

--- assets/scripts/lightning_strike.gd
+++ assets/scripts/lightning_strike.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
+var state_checker;
+
+func _ready() -> void:
+	state_checker = get_tree().get_first_node_in_group("state_checker");
+
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	var collider = area.get_parent();
 	if collider.is_in_group("PLAYER"):
@@ -9,4 +14,6 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 			else:
 				collider.is_die();
 			collider.lives_left -= 1;
-			print(collider.lives_left);
+			visible = false;
+			await get_tree().create_timer(2.0).timeout;
+			state_checker.check_game_state();
